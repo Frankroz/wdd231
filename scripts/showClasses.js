45d9007468eb6d560295ea5496b67b6c0fa44d00
Frankroz <franjrm201518@hotmail.com>
2025-03-12 15:40:36 +0800

    More bugs fixed

--- scripts/showClasses.js
+++ scripts/showClasses.js
@@ -15,15 +15,31 @@ const header = document.querySelector("header");
 // Create a function for the menu icon
 menu_icon.addEventListener("click", () => {
   if (menu_icon.src.toLowerCase().includes("menu")) {
-    menu_icon.setAttribute("src", "../images/close_icon.png");
+    menu_icon.setAttribute("src", "./images/close_icon.png");
     menu.classList.add("show");
     menu.style.display = "flex";
   } else {
-    menu_icon.setAttribute("src", "../images/menu_icon.png");
+    menu_icon.setAttribute("src", "./images/menu_icon.png");
     menu.classList.remove("show");
     menu.style.display = "none";
   }
 });
+
+// Function to handle resize event
+const handleResize = () => {
+  if (window.innerWidth >= 874) {
+    menu.style.display = "flex";
+    menu.classList.remove("show");
+    menu_icon.style.display = "none";
+  } else {
+    menu.style.display = "none";
+    menu_icon.style.display = "block";
+  }
+};
+
+// Initial check and event listener for resize
+handleResize();
+window.addEventListener("resize", handleResize);
 
 // Filter the classes depending on the button pressed
 const filterClasses = (b) => {
